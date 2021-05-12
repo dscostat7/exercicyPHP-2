22d18aa1e2323795791e40a477d5e902bf49b3e4
Diego_Souza <diego.s.costa@fiesc.com.br>
2021-05-12 08:56:44 -0300

    First commit

---
+++ array.php
@@ -0,0 +1,9 @@
+<?php
+
+$idade = [0, 1, 5, 15, 95, 84, 54, 19, 24];
+
+$idade[5] = 24;
+$idade[0] = 10;
+
+echo $idade[0] . ", " . $idade[5] . PHP_EOL;
+echo $idade . PHP_EOL;
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Exercitanto o básico do PHP e salvando repositório
+
+> Alura
+>> 2º Curso formação PHP
+____
+
+``` 
+>> Rumo à cerficação de especialista em PHP!
